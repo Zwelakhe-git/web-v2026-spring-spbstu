29b3eaebfb6dd1bf40a0c6e010070cb075d34f95
Karmanova Alyona <karaliona2006@gmail.com>
2026-04-02 20:24:02 +0300

    change == to ===

--- alyona.karmanova/task-2/lab2.js
+++ alyona.karmanova/task-2/lab2.js
@@ -2,7 +2,7 @@ function isBalanced(str) {
     let arr = [];
     let flag = true;
     for (let i = 0; i < str.length; i++) {
-        if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
             arr.push(str[i]);
         } else {
             if (arr.length === 0) {
@@ -10,9 +10,9 @@ function isBalanced(str) {
                 break;
             }
             let popedElem = arr.pop();
-            if (popedElem == '(' && str[i] != ')'
-                || popedElem == '{' && str[i] != '}'
-                || popedElem == '[' && str[i] != ']'
+            if (popedElem === '(' && str[i] != ')'
+                || popedElem === '{' && str[i] != '}'
+                || popedElem === '[' && str[i] != ']'
             ) {
                 flag = false;
                 break;
@@ -20,7 +20,7 @@ function isBalanced(str) {
         }
     }
 
-    if (flag && arr.length == 0) {
+    if (flag && arr.length === 0) {
         console.log(str + " correct");
     } else {
         console.log(str + " incorrect");
