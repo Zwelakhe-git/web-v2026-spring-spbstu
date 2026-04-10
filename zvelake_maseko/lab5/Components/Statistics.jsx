d74f56fe6affb3e7f23c82e9c85bd4b49b7b0a5e
Zwelakhe-git <zweklakhe.mzwet@gmail.com>
2026-04-10 19:39:14 +0300

    Refactor Statistics component and update navigation

--- zvelake_maseko/lab5/Components/Statistics.jsx
+++ zvelake_maseko/lab5/Components/Statistics.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import Chart from "./Chart";
 
 export default function Statistics({
-    prevSection,
     setCurrentSection, 
-    setPrevSection, 
     transactionsList
 }){
-    return (<div class="stats-section">
+    return (<div className="stats-section">
         <div className="section-info">
-            <h1 className="section-title">Statistics</h1>
+            <h1 className="section-title">Граф</h1>
         </div>
         <div className="section">
             <div className="section-nav">
                 <ul className="section-nav-links">
-                    <li className="section-nav-links__item" onClick={()=>{setPrevSection(2);setCurrentSection(prevSection)}}>Назад</li>
-                    <li className="section-nav-links__item" onClick={()=>{setPrevSection(2);setCurrentSection(0)}}>На Главную</li>
+                    <li className="section-nav-links__item" onClick={()=>{setCurrentSection(0)}}>На Главную</li>
                 </ul>
             </div>
             <div className="section-body">
